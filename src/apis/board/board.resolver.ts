143664010c131b7300ff43b726b7675a1318387f
KurtDev599 <sw536880@naver.com>
2022-12-18 20:51:02 +0900

    GraphQL 적용

--- src/apis/board/board.resolver.ts
+++ src/apis/board/board.resolver.ts
@@ -1,12 +1,20 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { BoardService } from './board.service';
+import { Board } from './entities/board.entity';
+import { CreateBoardInput } from './dto/createBoard.input';
 
 @Resolver()
 export class BoardResolver {
   constructor(private readonly boardService: BoardService) {}
 
-  @Query(() => String)
-  getHello() {
-    return this.boardService.aaa();
+  @Query(() => [Board])
+  findAllBoards() {
+    return this.boardService.findAll();
+  }
+
+  @Mutation(() => String)
+  createBoard(@Args('createBoardInput') createBoardInput: CreateBoardInput) {
+    console.log(createBoardInput);
+    return this.boardService.createBoard();
   }
 }
--- src/apis/board/board.service.ts
+++ src/apis/board/board.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BoardService {
-  aaa() {
-    return 'hello world';
+  findAll() {
+    const result = [
+      { number: 1, writer: '철수', title: '제목', contents: '내용' },
+      { number: 2, writer: '영희', title: '제목', contents: '내용' },
+      { number: 3, writer: '훈이', title: '제목', contents: '내용' },
+    ];
+
+    return result;
+  }
+
+  createBoard() {
+    return 'success';
   }
 }
