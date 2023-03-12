2e117c1bf0cdba51c81a5dc5b8e682cc81893855
swkim <sw536880@finset.io>
2023-03-12 22:04:03 +0900

    create test code

--- src/apis/auth/auth.controller.ts
+++ src/apis/auth/auth.controller.ts
@@ -1,12 +1,29 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { UserService } from '../users/user.service';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
   @Get('/login/google')
   @UseGuards(AuthGuard('google'))
   // google login
-  loginGoogle() {
+  async loginGoogle() {
+    const user = await this.userService.findOneUserEmail({ email: '' });
+
+    if (!user) {
+      this.userService.createUser({
+        email: 'email',
+        password: 'password',
+        name: 'name',
+        age: 1,
+      });
+    }
+
     return null;
   }
 }
