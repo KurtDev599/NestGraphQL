6d6e3c578d4e6c3e2584fe4f398e8775704be87a
KurtDev599 <sw536880@naver.com>
2023-03-19 20:37:31 +0900

    Google login in progress

--- src/apis/auth/auth.controller.ts
+++ src/apis/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserService } from '../users/user.service';
 import { AuthService } from './auth.service';
+import { User } from '../users/entities/user.entity';
+
+interface IOAuthUser {
+  user: Pick<User, 'email' | 'password' | 'name' | 'age'>;
+}
 
 @Controller('auth')
 export class AuthController {
@@ -9,22 +14,25 @@ export class AuthController {
     private readonly userService: UserService,
     private readonly authService: AuthService,
   ) {}
+
   @Get('/login/google')
   @UseGuards(AuthGuard('google'))
   // google login
-  async loginGoogle() {
-    const user = await this.userService.findOneUserEmail({ email: '' });
+  async loginGoogle(@Req() req: Request & IOAuthUser, @Res() res: Response) {
+    const user = await this.userService.findOneUserEmail({
+      email: req.user.email,
+    });
 
     if (!user) {
       this.userService.createUser({
-        email: 'email',
-        password: 'password',
-        name: 'name',
-        age: 1,
+        email: req.user.email,
+        password: req.user.password,
+        name: req.user.name,
+        age: req.user.age,
       });
     }
 
-    // this.authService.setRefreshToken({ user, res })
+    this.authService.setRefreshToken({ user, res });
     // res.redirect('http://localhost:8080/src/apis/auth/test.html');
 
     return null;
